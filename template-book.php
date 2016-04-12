5d8ca44a3cc39aa97ec7a48bfbe6a48bfa0d214a
Greg Opperman <gopperman@gmail.com>
2016-04-11 23:45:48 -0400

    Book page improvements

--- template-book.php
+++ template-book.php
@@ -4,23 +4,34 @@
  */
 while ( have_posts() ) : the_post();
   $id = get_the_ID();
+  $hero = get_post_meta( $id, 'content_header')[0];
   $excerpts = get_post_meta( $id, 'book_excerpts' )[0];
   $testimonials = get_post_meta( $id, 'book_testimonials' )[0];
+  $worksheet_instructions = get_post_meta( $id, 'book_worksheet_instructions' )[0];
   $worksheets = get_post_meta( $id, 'book_worksheets')[0];
 ?>
 
   <div id="content-header" class="hero">
     <div class="container-fluid">
-      <div class="col-xs-3">Book Photo</div>
-      <div class="col-xs-8 col-xs-push-1">Featured Testimonial</div>
+      <div class="col-sm-4">
+        <img src="<?php echo esc_url( wp_get_attachment_image_src( $hero['image'], 'full' )[0] ); ?>" />
+      </div>
+      <div class="col-sm-7 col-sm-offset-1">
+        <div class="hero__description">
+          <?php echo wp_kses_post( $hero['description'] ); ?>
+        </div>
+        <a href="<?php echo esc_url( $hero['ctas']['link'] ); ?>" class="button__cta">
+          <?php echo wp_kses_post( $hero['ctas']['text'] ); ?>
+        </a>
+      </div>
     </div>
   </div>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-8">
-        <?= get_the_content(); ?>
+      <div class="col-sm-8">
+        <?= the_content(); ?>
       </div>
-      <div class="col-md-4 excerpts">
+      <div class="col-sm-4 excerpts">
         <h3>Excerpts</h3>
         <?php foreach ( $excerpts as $excerpt ) { ?>
           <h4><a href="<?php echo esc_url( get_permalink( $excerpt['excerpt_link'] ) ); ?>"><?php echo wp_kses_post( $excerpt['chapter'] ); ?>:</a></h4>
@@ -30,27 +41,30 @@ while ( have_posts() ) : the_post();
         <?php } ?>
       </div>
     </div>
-    <div class="row">
-      <h4>What People Are Saying</h4>
-    </div>
-    <div class="row">
-      <?php foreach ( $testimonials as $testimonial ) { ?>
-        <div class="col-sm-4"><?php echo wp_kses_post( $testimonial['testimonial'] ); ?></div>
-      <?php } ?>
+    <div class="testimonials">
+      <div class="row">
+        <h4>What People Are Saying</h4>
+      </div>
+      <div class="row">
+        <?php foreach ( $testimonials as $testimonial ) { ?>
+          <div class="col-sm-4"><?php echo wp_kses_post( $testimonial['testimonial'] ); ?></div>
+        <?php } ?>
+      </div>
     </div>
   </div>
   <div class="worksheets">
     <div class="container-fluid">
       <h3>Worksheets</h3>
-      <p>Simple instructions</p>
+      <p><?php echo wp_kses_post( $worksheet_instructions ); ?></p>
       <div class="row">
         <?php foreach ( $worksheets as $ws ) { ?>
           <div class="col-sm-6 col-md-3">
+            <a href="<?php esc_url( get_permalink( $ws['worksheet_link'] ) ); ?>" class="worksheet__link">
             <?php if ( has_post_thumbnail( $ws['worksheet_link'] ) ) {
-              $image = wp_get_attachment_image_src( $ws['worksheet_link'], 'single-post-thumbnail' ); ?>
+              $image = wp_get_attachment_image_src( get_post_thumbnail_id( $ws['worksheet_link'] ), 'single-post-thumbnail' ); ?>
               <img src="<?php echo esc_url( $image[0] ); ?>" alt="<?php echo esc_attr( get_the_title( $ws['worksheet_link'] ) ); ?>"/>
             <?php } ?>
-            <a href="<?php esc_url( get_permalink( $ws['worksheet_link'] ) ); ?>" class="worksheet__link"><?php echo wp_kses_post( get_the_title( $ws['worksheet_link'] ) ); ?></b></a>
+            <b><?php echo wp_kses_post( get_the_title( $ws['worksheet_link'] ) ); ?></b></a>
           </div>
         <?php } ?>
       </div>
--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,5 @@
 <footer class="content-info">
   <div class="container">
-    <p>&copy; Jonathan Fader. By <a href="http://gopperman.com">gopperman</a>.</p>
+    <p>&copy; Jonathan Fader. All Rights Reserved. By <a href="http://gopperman.com">gopperman</a>.</p>
   </div>
 </footer>
