a8356a179655d0d2255ab0c96cc9caccf5dc5009
Greg Opperman <gopperman@gmail.com>
2016-04-12 01:46:40 -0400

    Homepage

--- front-page.php
+++ front-page.php
@@ -2,22 +2,47 @@
   $id = get_the_ID();
   $header = get_post_meta( $id, 'content_header' )[0];
   $about = get_post_meta( $id, 'homepage_about' )[0];
-  $content = get_post_meta( $id, 'homepage_content');
-  $product = get_post_meta( $id, 'homepage_product' );
-  var_dump( $product );
+  $content = get_post_meta( $id, 'homepage_content')[0];
+  $product = get_post_meta( $id, 'homepage_product' )[0];
 ?>
-<div id="content-header" class="hero" style="background:url(<?php echo esc_url( wp_get_attachment_url( $header['image'] ) ); ?>);">
+<div id="content-header" class="hero">
   <div class="container">
-    <h1><?php echo wp_kses_post( $header['header_text'] ); ?></h1>
-    <h2><?php echo wp_kses_post( $header['header_description'] ); ?></h2>
-    <?php if ( isset( $header['ctas'] ) ) {
-      foreach ( $header['ctas'] as $cta ) { ?>
-        <a class="cta" href="<?php echo esc_url( get_permalink( $cta['link'] ) ); ?>"><?php echo wp_kses_post( $cta['text'] ); ?></a>
-      <?php }
-    } ?>
+    <div class="col-sm-6">
+      <h1><?php echo wp_kses_post( $header['header_text'] ); ?></h1>
+      <h2><?php echo wp_kses_post( $header['header_description'] ); ?></h2>
+      <?php if ( isset( $header['ctas'] ) ) {
+        foreach ( $header['ctas'] as $cta ) { ?>
+          <a class="button__cta button__cta-purple" href="<?php echo esc_url( get_permalink( $cta['link'] ) ); ?>"><?php echo wp_kses_post( $cta['text'] ); ?></a>
+        <?php }
+      } ?>
+    </div>
   </div>
 </div>
-<div
-<div id="about" class="container">
-  <p><?php echo wp_kses_post( $about ); ?></p>
+<div class="content-areas row">
+  <?php foreach ( $content as $area ) { ?>
+  <div class="col-md-4 content__area">
+    <div class="content__area-title"><?php echo wp_kses_post( $area['header_text'] ); ?></div>
+
+    <div class="hidden-md content__area-description">
+      <h2><?php echo wp_kses_post( $area['header_description'] ); ?></h2>
+      <a class="button__cta" href="<?php echo esc_url( get_the_permalink( $area['cta']['link'] ) ); ?>"><?php echo wp_kses_post( $area['cta']['text'] ); ?></a>
+    </div>
+  </div>
+  <? } ?>
+</div>
+<div class="product">
+  <div class="container-fluid">
+    <div class="col-sm-4">
+      <img src="<?php echo esc_url( wp_get_attachment_image_src( $product['image'], 'full' )[0] ); ?>" />
+    </div>
+    <div class="col-sm-7 col-sm-offset-1">
+      <div class="hero__description">
+        <h3> <?php echo wp_kses_post( $product['header_text'] ); ?></h3>
+        <?php echo wp_kses_post( $product['header_description'] ); ?>
+      </div>
+      <a href="<?php echo esc_url( get_the_permalink( $product['cta']['link'] ) ); ?>" class="button__cta button__cta-purple">
+        <?php echo wp_kses_post( $product['cta']['text'] ); ?>
+      </a>
+    </div>
+  </div>
 </div>
