6682ebf145698c2e627b509378e32e751c5321d2
Greg Opperman <gopperman@gmail.com>
2016-04-17 21:27:29 -0400

    Closes #12

--- templates/footer.php
+++ templates/footer.php
@@ -9,7 +9,6 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h2 class="modal-title">Contact Us</h2>
       </div>
       <div class="modal-body">
         <?php echo do_shortcode( '[contact-form-7 id="1173" title="Contact Dr. Fader"]' ); ?>
