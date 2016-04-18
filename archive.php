dacbda4840f54eaaefc08d33eb3b1ebbb1b5dbf3
Greg Opperman <gopperman@gmail.com>
2016-04-17 23:15:38 -0400

    closes #23

--- archive.php
+++ archive.php
@@ -8,8 +8,8 @@
       <?php while (have_posts()) : the_post(); ?>
         <?php get_template_part('templates/content', 'excerpt'); ?>
       <?php endwhile; ?>
-      <div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
-      <div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
+      <div class="nav-previous button__cta button__cta-purple alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
+      <div class="nav-next button__cta button__cta-purple alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
     </main>
     <aside class="col-md-5">
       <h4>Post Categories</h4>
