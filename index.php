2b1c0f941e8cf6b00e0ebde6d700cdfe7a4811eb
Greg Opperman <gopperman@gmail.com>
2016-04-24 19:58:28 -0400

    Check to see if previous and next before adding html. Closes #45

--- index.php
+++ index.php
@@ -5,8 +5,16 @@
       <?php while (have_posts()) : the_post(); ?>
         <?php get_template_part('templates/content', 'excerpt'); ?>
       <?php endwhile; ?>
-      <div class="nav-previous button__cta button__cta-purple alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
-      <div class="nav-next button__cta button__cta-purple alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
+      <div class="row">
+        <div class="col-xs-12">
+          <?php if ( $prev = get_previous_posts_link() ) { ?>
+            <div class="nav-next button__cta button__cta-purple alignleft"><?= $prev; ?></div>
+          <?php } ?>
+          <?php if ( $next = get_next_posts_link() ) { ?>
+            <div class="nav-previous button__cta button__cta-purple alignright"><?= $next; ?></div>
+          <? } ?>
+        </div>
+      </div>
     </main>
     <aside class="col-md-5">
       <h4>Post Categories</h4>
