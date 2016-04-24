255e82abee2f374a3d4f75320b851fe7f01f0666
Greg Opperman <gopperman@gmail.com>
2016-04-24 19:33:42 -0400

    Closes #35

--- functions.php
+++ functions.php
@@ -44,3 +44,17 @@ function oembed_video_container( $html, $url, $attr, $post_id ) {
 }
 
 add_filter( 'embed_oembed_html', 'oembed_video_container', 99, 4 );
+
+/**
+ * Remove taxonomy lavels from archive titles
+ */
+add_filter( 'get_the_archive_title', function ($title) {
+  if ( is_category() ) {
+    $title = single_cat_title( '', false );
+  } elseif ( is_tag() ) {
+    $title = single_tag_title( '', false );
+  } elseif ( is_author() ) {
+    $title = '<span class="vcard">' . get_the_author() . '</span>' ;
+  }
+  return $title;
+});
