5a3cbb27c4c60cde09be867d8b365aefd847f0d9
Greg Opperman <gopperman@gmail.com>
2016-08-22 22:57:16 -0400

    Practice area style

--- template-practice-areas.php
+++ template-practice-areas.php
@@ -18,29 +18,33 @@ while ( have_posts() ) : the_post();
   <div class="practice-areas">
     <div class="container-fluid">
       <div class="row">
-        <div class="col-lg-3">
+        <div class="col-md-4 col-lg-3">
           <h3>Practice Areas</h3>
           <ul class="practice-areas__nav">
-            <?php foreach ($practice_areas as $area) { ?>
-              <li class="practice-areas__nav--item"><a class="practice-areas__nav--link"><?php echo wp_kses_post( $area['title'] ); ?></a>
-            <?php } ?>
+            <?php $first = true;
+            foreach ($practice_areas as $area) { ?>
+              <li class="practice-areas__nav--item<?php if ($first) echo ' practice-areas__nav--item-active'; ?>"><?php echo wp_kses_post( $area['title'] ); ?>
+            <?php $first = false;
+            } ?>
           </ul>
         </div>
-        <div class="col-lg-9">
-          <?php foreach ($practice_areas as $area) { ?>
-            <div class="practice-areas__section">
+        <div class="col-md-8 col-lg-9 practice-areas__container">
+          <?php $first = true;
+          foreach ($practice_areas as $area) { ?>
+            <div class="practice-areas__section<?php if ($first) echo ' practice-areas__section--active'; ?>">
               <h3 class="practice-areas__title"><?php echo wp_kses_post( $area['title'] ); ?></h3>
               <?php echo wp_kses_post( $area['content']); ?>
               <p><a href="<?php echo esc_url( get_permalink( $area['link'] ) ); ?>">Learn More about <?php echo wp_kses_post( $area['title'] ); ?></a></p>
             </div>
-          <?php } ?>
+          <?php $first = false;
+          } ?>
         </div>
       </div>
     </div>
   </div>
-  <div class="featured_posts">
+  <div class="featured-posts">
     <div class="container-fluid">
-      <h3>Featured Blog Posts</h3>
+      <h4>Featured Blog Posts</h4>
       <div class="row">
         <?php foreach ( $featured_posts as $featured_post ) { ?>
           <div class="col-md-4">
@@ -56,22 +60,4 @@ while ( have_posts() ) : the_post();
       </div>
     </div>
   </div>
-<!--  <div class="worksheets">
-    <div class="container-fluid">
-      <h3>Resources</h3>
-      <p><?php echo wp_kses_post( $worksheet_instructions ); ?></p>
-      <div class="row">
-        <?php foreach ( $worksheets as $ws ) { ?>
-          <div class="col-sm-6 col-md-3">
-            <a href="<?php echo esc_url( get_permalink( $ws['worksheet_link'] ) ); ?>" class="worksheet__link">
-            <?php if ( has_post_thumbnail( $ws['worksheet_link'] ) ) {
-              $image = wp_get_attachment_image_src( get_post_thumbnail_id( $ws['worksheet_link'] ), 'single-post-thumbnail' ); ?>
-              <img src="<?php echo esc_url( $image[0] ); ?>" alt="<?php echo esc_attr( get_the_title( $ws['worksheet_link'] ) ); ?>"/>
-            <?php } ?>
-            <b><?php echo wp_kses_post( get_the_title( $ws['worksheet_link'] ) ); ?></b></a>
-          </div>
-        <?php } ?>
-      </div>
-    </div>
-  </div>-->
 <?php endwhile; ?>
